d05651b9a4ab82e58e17cf214b0ead20fd40c21c
Susie Douang <soutsada.d@gmail.com>
2024-03-06 18:10:09 -0600

    start on stretch exercises

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -31,6 +31,13 @@ let listItems = (basket) => {
 // Test the functions and display basket items
 console.log(listItems(['shoes', 'hat', 'jacket'])); // true
 
+// Example test
+console.log(`Basket is ${basket}`);
+console.log('Adding apples (expect true)', addItem('apples'));
+console.log(`Basket is now ${basket}`);
+
+// ----------------------------------------
+
 // empty function to make items disappear
 let empty = (basket) => {
   // check if the basket has items
@@ -43,6 +50,28 @@ let empty = (basket) => {
 
 // Test the functions and display no items
 console.log(empty(['shoes', 'hat', 'jacket']));
+
+// ----------------------------------------
+
+// STRETCH
+// Add a global const named maxItems and set it to 5.
+const maxItems = 5;
+
+// Create a function called isFull().
+let isFull = (items) => {
+  // check the items
+  // console.log('items length', items);
+  // check how many items
+  // console.log('items length', items.length);
+  if (items.length >= maxItems) {
+    return true;
+  } else {
+    return false;
+  }
+};
+// Test max items
+// console.log(isFull(['shoes', 'jacket', 'shirt', 'hat', 'gloves'])); // full, true
+console.log(isFull(['shorts', 'pants', 'skirt'])); // not full, false
 
 // DO NOT MODIFY
 // Used for automated testing
